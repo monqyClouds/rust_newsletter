647f7524662264c15469deab62b1296a3fd77577
Somto <somto.onyebuchi@castlestash.com>
2023-08-27 22:00:32 -0700

    add unit tests for subscriber name input validation

--- src/domain.rs
+++ src/domain.rs
@@ -5,6 +5,7 @@ pub struct NewSubscriber {
     pub name: SubscriberName,
 }
 
+#[derive(Debug)]
 pub struct SubscriberName(String);
 
 impl SubscriberName {
@@ -35,8 +36,40 @@ mod tests {
     use claim::{assert_err, assert_ok};
 
     #[test]
-    fn a_s56_grapheme_long_name_is_valid() {
+    fn a_256_grapheme_long_name_is_valid() {
         let name = "a".repeat(256);
+        assert_ok!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn a_name_longer_than_256_grapheme_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let name = String::from(" ");
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let name = "".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn names_containing_an_invalid_character_are_rejected() {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
+            let name = name.to_string();
+            assert_err!(SubscriberName::parse(name));
+        }
+    }
+
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        let name = String::from("Da Vinci");
         assert_ok!(SubscriberName::parse(name));
     }
 }
